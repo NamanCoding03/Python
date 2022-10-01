ca27cc22ec43a3814867187d432400b13be2c222
Naman Jain <114656612+NamanCoding03@users.noreply.github.com>
2022-10-01 19:11:57 +0530

    Create SI.py

---
+++ DSA/Basic Codes/SI.py
@@ -0,0 +1,6 @@
+principal = int(input("Enter the principal interest"))
+rate = float(input("Enter the rate of interest"))
+time = int(input("Enter the time for which SI needs to be calculated"))
+si = principal*rate*time/100
+
+print(si)
